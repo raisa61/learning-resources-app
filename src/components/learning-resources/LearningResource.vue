ef9e94c917f98ae79ad0906fb8bba9ce7a7f0fe9
Raisa Zaman <raisazaman2000@gmail.com>
2023-07-13 10:14:38 -0400

    outputting dummy data

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -7,7 +7,7 @@
       </header>
     </div>
     <p>{{ description }}</p>
-    <nav><a :href="link"></a></nav>
+    <nav><a :href="link">View Resources</a></nav>
   </li>
 </template>
 
@@ -16,3 +16,35 @@
     props: ['title', 'description', 'link']
   }
 </script>
+
+<style>
+  li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
